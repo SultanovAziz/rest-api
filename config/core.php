fcc0f5fab99e43103f4d3b44efa1d9ab0ed7b77c
Admin <aziz89131983189@gmail.com>
2022-03-25 01:36:23 +0700

    add frontend js jquery and other files end project

--- config/core.php
+++ config/core.php
@@ -1,1 +1,1 @@
-<?phpini_set('display_errors', 1);error_reporting(E_ALL);$home_url="http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";$page = isset($_GET['page']) ? $_GET['page'] : 1;// init quantity records in page$records_per_page = 5;$from_record_num = ($records_per_page * $page) - $records_per_page;
+<?phpini_set('display_errors', 1);error_reporting(E_ALL);$home_url="http://rest-api/";$page = isset($_GET['page']) ? $_GET['page'] : 1;// init quantity records in page$records_per_page = 5;$from_record_num = ($records_per_page * $page) - $records_per_page;
