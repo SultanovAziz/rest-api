9806a62e346663af83b28f9582f53d73fde1fb57
Admin <aziz89131983189@gmail.com>
2022-03-24 14:33:39 +0700

    add product object and function create and read this object

--- pattern/TSingeltone.php
+++
@@ -1,1 +0,0 @@
-<?phptrait TSingeltone{    public static $instance;    public static function instance()    {        if (self::$instance == null){            self::$instance = new self();        }        return self::$instance;    }}
